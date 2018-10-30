83cf5711497912640ae83db21962d278e41df61a
Hai Nguyen <nguyendachai@hotmail.com>
2018-10-30 21:52:02 +0700

    Created to Megus.API and Megus.Client

---
+++ Megus.Client/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Megus.Client.Models;
+
+namespace Megus.Client.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult CategoriesProductView()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
